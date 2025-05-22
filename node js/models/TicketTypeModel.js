2e6e8e501c674687d5dfefcd5fd3ac4be1933bdf
sari-dev-1 <a0583214067@gmail.com>
2025-05-23 00:34:21 +0300

    refactor: update ticket type model that include category

--- node js/models/TicketTypeModel.js
+++ node js/models/TicketTypeModel.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
-//const { options } = require('../Routers/toyRouter');
 
-const TicketTypeModel = mongoose.Schema({
-name:{ type: String,
-    enum: ['permissions', 'changes', 'serverFarmInquiries'], 
-    required: true },
-//options: [{ type: String }] 
+const TicketTypeModel = new mongoose.Schema({
+  name: { 
+    type: String, 
+    enum: ['permissions', 'changes', 'database_access', 'file_directory_access', 'code_help', 'technical_help', 'change_request', 'other'], 
+    required: true 
+  },
+  category: { 
+        type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category', 
+    required: true,
+  }
 });
-module.exports = mongoose.model("TicketType",TicketTypeModel)
+
+module.exports = mongoose.model("TicketType", TicketTypeModel);
