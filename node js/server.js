ab4b57cf1aea4551e735591d45f43065ff32c341
sari-dev-1 <a0583214067@gmail.com>
2025-05-25 01:17:50 +0300

    Modify server setup and middleware configuration

--- node js/server.js
+++ node js/server.js
@@ -1,56 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const passport = require("passport");
-// const session = require("express-session"); // משתמשים ב-express-session במקום cookie-session
-
-// // התחברות לקובץ סיסמאות
-// dotenv.config();
-
-// // יצירת האפליקציה
-// const app = express();
-// app.use(express.json());
-
-// // קונפיגורציה ל־passport
-// require("./config/passport");
-
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// // משתמשים ב-express-session במקום cookie-session
-// app.use(session({
-//   secret: 'secretkey', // מפתח הצפנה (מומלץ לשנות אותו ליותר מאובטח בסביבה אמיתית)
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: false } // אם יש שימוש ב-HTTPS, יש לשנות ל-true
-// }));
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // התחברות למסד נתונים
-// mongoose.connect(process.env.DB_URL)
-//   .then(() => console.log("Connected…"))
-//   .catch(err => console.error("Connection failed…"));
-
-// // ראוטים
-// const responseRouter = require("./routers/responseRouter");
-// const ticketTypeRouter = require("./routers/ticketTypeRouter");
-// const userRouter = require("./routers/userRouter");
-// const authRouter = require("./routers/authRouter");
-
-// app.use("/response", responseRouter);
-// app.use("/TicketType", ticketTypeRouter);
-// app.use("/User", userRouter);
-// app.use("/auth", authRouter); // ראוט של התחברות רגילה + גוגל
-// app.get("/", (req, res) => res.send("Server is running"));
-
-// app.listen(process.env.PORT, () =>
-//   console.log(`Server running on http://localhost:${process.env.PORT}`)
-// );
-
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -65,6 +12,7 @@ const ticketTypeRouter = require("./routers/ticketTypeRouter");
 const userRouter = require("./routers/userRouter");
 const authRouter = require("./routers/authRouter");
 const sendEmail = require("./routers/emailRouter");
+const categoryRouter = require('./routers/categoryRouter');
 
 dotenv.config();
 const app = express();
@@ -89,12 +37,12 @@ mongoose.connect(process.env.DB_URL)
   .then(() => console.log("Connected…"))
   .catch(err => console.error("Connection failed…"));
 
-// ראוטים
 app.use("/response", responseRouter);
 app.use("/TicketType", ticketTypeRouter);
 app.use("/User", userRouter);
 app.use("/auth", authRouter);
 app.use("/Email", sendEmail);
+app.use('/Categories', categoryRouter);
 
 app.get("/", (req, res) => res.send("Server is running"));
 
